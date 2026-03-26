441b5b6333303fe3067a742d475448b124b54173
jamstart36 <jamsiandm@gmail.com>
2026-03-26 12:44:46 +0800

    a

--- src/components/NamidoriPos.jsx
+++ src/components/NamidoriPos.jsx
@@ -50,7 +50,7 @@ export const NamidoriPos = ({ onLogout, user }) => {
           .map((c) => updateStock(c.originalId || c.id, Math.max(0, c.stock - c.qty)));
 
         const order = {
-          items: cart.map((c) => c.name).join(", "),
+          items: cart.map((c) => `${c.qty > 1 ? `x${c.qty} ` : ""}${c.name}`).join(", "),
           item_details: JSON.stringify(
             cart.flatMap((c) => {
               const details = [];
